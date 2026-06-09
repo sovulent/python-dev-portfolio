32ec7b7b1f92812d7035a8a61d2b867111f25aad
sovulent <sovulent@gmail.com>
2026-06-09 18:04:39 -0500

    Adding the average expense calculation

--- expense-tracker/expense_tracker.py
+++ expense-tracker/expense_tracker.py
@@ -1,20 +1,27 @@
+# Create an empty list that stores expense ammounts
 expenses = []
 
-amount = input("Enter expense: ")
+# Ask the user for three expenses and add them to the list [1-3]
+amount = input("Enter expense 1: ")
 amount = float(amount)
-
 expenses.append(amount)
 
-amount = input("Enter expense: ")
+amount = input("Enter expense 2: ")
 amount = float(amount)
-
 expenses.append(amount)
 
-amount = input("Enter expense: ")
+amount = input("Enter expense 3: ")
 amount = float(amount)
-
 expenses.append(amount)
 
-total=sum(expenses)
+# Calculate the total cost of all expenses + print the result
+total= sum(expenses)
+print("Total expenses:",total)
 
-print("Total expenses: ",total)
+# Count how many expenses were entered + print the result
+expense_count = len(expenses)
+print("Number of expenses:",expense_count)
+
+# Calculate the average expense + print the result
+average = total / expense_count
+print("Average expense:",average)
